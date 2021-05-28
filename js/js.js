57adf74889d4a39a73096c6a137c53aa01d8b904
Алексей Слаутин <Slaytin2014@yandex.ru>
2021-05-28 18:21:20 +0300

    Сделал несколько вариантов Усложненного Задания

--- js/js.js
+++ js/js.js
@@ -8,3 +8,27 @@ console.log(composition);
 composition = composition ** 3;
 console.log(composition);
 console.log(String(composition).slice(0, 2));
+
+//Задание 2 вариант 2
+let square = function sumDigits(num) {
+    let str = num.toString();
+    let sum = 1;
+  
+    for (let i = 0; i < str.length; i++) {
+      sum *= parseInt(str.charAt(i), 10);
+    }
+  
+    console.log(sum);
+    composition2 = sum ** 3;
+    console.log(composition2);
+    console.log(String(composition2).slice(0, 2));
+  }
+
+square(num);
+
+//Задание 2 вариант 3
+numArray2 = num.toString().split('').reduce((a, b)=>a * b);
+console.log(numArray2);
+numArray2 = numArray2 ** 3;
+console.log(numArray2);
+console.log(String(numArray2).slice(0, 2))
