a205fc425af287f23f4234f475c1b3e4e01ea743
Алексей Слаутин <Slaytin2014@yandex.ru>
2021-05-28 17:30:23 +0300

    Выполнил задание вариант 1

--- js/js.js
+++ js/js.js
@@ -1,1 +1,10 @@
 let num = 266219;
+
+//Задание 2 вариант 1
+let numArray = num.toString().split('').map(Number);
+console.log(numArray);
+let composition = numArray[0] * numArray[1] * numArray[2] * numArray[3] * numArray[4] * numArray[5];
+console.log(composition);
+composition = composition ** 3;
+console.log(composition);
+console.log(String(composition).slice(0, 2));
