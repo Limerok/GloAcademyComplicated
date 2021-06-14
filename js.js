bcae7adfdb5efc84315ee5b161ce16cd33accad5
Алексей Слаутин <Slaytin2014@yandex.ru>
2021-06-14 22:41:06 +0300

    Сделал первую часть

--- js.js
+++ js.js
@@ -1,96 +1,71 @@
 'use strict';
 
-const todoControl = document.querySelector('.todo-control'),
-    headerInput = document.querySelector('.header-input'),
-    todoList = document.querySelector('.todo-list'),
-    todoCompleted = document.querySelector('.todo-completed'); 
+let cssText = document.querySelector('.css-text'),
+    selectorText = document.querySelector('.selector-text'),
+    cssStyleHeight = document.querySelector('.css-style-height'),
+    listStyleWidth = document.querySelector('.list-style-width'),
+    listStyleBg = document.querySelector('.list-style-bg'),
+    listStyleFontSize = document.querySelector('.list-style-fontSize'),
+    btnStar = document.getElementById('btn-start');
 
-let todoData = [
-    /* {value: "sdfsdf", completed: false},
-    {value: "sd23", completed: false} */
-];
-let mass = JSON.parse(localStorage.getItem("myCase"));
-
-//JSON.parse(localStorage.getItem("arr"));
-
-
-const render = function() {
-    mass = JSON.parse(localStorage.getItem("myCase"));
+function DomElement () {
+    this.selector = selectorText;
+    this.height = cssStyleHeight;
+    this.width = listStyleWidth;
+    this.bg = listStyleBg;
+    this.fontSize = listStyleFontSize;
+}
+DomElement.prototype.render = function() {
+    let val = this.selector.value;
+    let valInd = val.charAt(0);
     
-    if (mass === null) {
-        todoList.textContent = '';
-        todoCompleted.textContent = '';
-        return;
+    if ( valInd === '.') {
+        const newElement = document.createElement('div');
+        newElement.classList.add(val.slice(1));
+        cssText.insertAdjacentElement('afterbegin', newElement);
+    }else if (valInd === '#') {
+        const newElement = document.createElement('div');
+        newElement.setAttribute('id', val.slice(1));
+        cssText.insertAdjacentElement('afterbegin', newElement);
     } else {
-        todoData=mass;
-        localStorage.setItem('myCase', JSON.stringify(todoData));
-
-        todoList.textContent = '';
-        todoCompleted.textContent = '';
-
-        todoData.forEach(function(item, i){
-        
-            const li = document.createElement('li');
-            li.classList.add('todo-item');
-    
-            li.innerHTML = '<span class="text-todo">' +  item.value + '</span>' +
-                '<div class="todo-buttons">' +
-                    '<button class="todo-remove"></button>' +
-                    '<button class="todo-complete"></button>' +
-                '</div>';
-            if (item.completed){
-                todoCompleted.append(li);
-            } else {
-                todoList.append(li);
-            }
-            
-            const btntodoCompleted = li.querySelector('.todo-complete');//Кнопа выполнено
-            btntodoCompleted.addEventListener('click', function() {
-                item.completed = !item.completed;
-                localStorage.setItem('myCase', JSON.stringify(todoData));
-                render();
-            });
-    
-            const btnTodoRemove = li.querySelector('.todo-remove'); //Кнопа удалить
-            btnTodoRemove.addEventListener('click', function() {
-               todoData.splice(i,1);
-               localStorage.setItem('myCase', JSON.stringify(todoData));
-               render();
-            });
-            
-    
-            
-           // localStorage.setItem('arr', JSON.stringify(mass));
-        });
-
-
+        alert('Нужна . или # перед буковками');
     }
+    this.renderStyle();
+};
+DomElement.prototype.height = function() {
 
-    
+};
+DomElement.prototype.renderStyle = function() {
+    let h = this.height.value,
+        w = this.width.value,
+        b = +this.bg.value,
+        a = this.fontSize.value;
+
+    let val = selectorText.value;
+    let newElem = document.querySelector(val);
+    newElem.textContent = 'JS';
+
+    newElem.style.height = h + 'px';
+    newElem.style.width = w + 'px';
+    if (b === 1) {
+        newElem.style.background = '#000000';
+    } else if (b === 2) {
+        newElem.style.background = '#0094ff';
+    } else if (b === 3) {
+        newElem.style.background = '#00ff04';
+    }
+    newElem.style.fontSize = a + 'px';
     
 
-    
+
 };
 
-todoControl.addEventListener('submit', function(event){
-    event.preventDefault();
+let obj = new DomElement();
 
-    if (headerInput.value !== '') {
-        
-        const newTodo = {
-            value: headerInput.value,
-            completed: false
-        };
-    
-        todoData.push(newTodo);
-        localStorage.setItem('myCase', JSON.stringify(todoData));
-        render();
-        headerInput.value = ''; //Очищаем строку после submit
-    } else {
-        return;
-    }
-    
+
+
+btnStar.addEventListener('click', function(){
+    obj.render();
 });
 
-
-render();
+//let blockValue = cssText.querySelector(selectorText.value);
