2901c924ac4b8fa0061b21e4e1a2e8a55163a063
Алексей <slaytin2014@yandex.ru>
2021-06-17 23:38:50 +0300

    Сделал дополнительное урок 14

--- js.js
+++ js.js
@@ -1,20 +1,48 @@
 'use strict';
 
-const background = document.querySelector('.backgraund'),
-    backgroundText = document.querySelector('.backgraund--text'),
-    backgroundBtn = document.querySelector('.background--btn');
+let bod = document.querySelector('body');
+console.log(bod);
 
-function random() {
-    let r = Math.floor(Math.random() * (256)),
-        g = Math.floor(Math.random() * (256)),
-        b = Math.floor(Math.random() * (256)),
-        color = '#' + r.toString(16) + g.toString(16) + b.toString(16);
-
-    background.style.background = color;
-    backgroundText.textContent = color;
+function DomElement (h, w) {
+    this.height = h;
+    this.width = w;
+    this.newElem = document.createElement('div');
+    this.top = 0;
+    this.left = 0;
 }
+DomElement.prototype.render = function() {
+    this.newElem.classList.add('cub');
+    console.log(this);
+    this.newElem.style.width = this.width + 'px';
+    this.newElem.style.height = this.height + 'px';
+    this.newElem.style.background = 'black';
+    this.newElem.style.position = 'absolute';
+    bod.insertAdjacentElement('afterbegin', this.newElem);
+    this.renderEvent();
+};
+DomElement.prototype.renderEvent = function() {
+    console.log(this);
+    let leftN = this.left;
+    let topN = this.top;
+    document.addEventListener('keydown', (event) => {
+        if (event.code === 'ArrowUp') {
+            topN = topN - 10;
+           this.newElem.style.top = topN + 'px';
+        }
+        if (event.code === 'ArrowDown') {
+            topN = topN + 10;
+            this.newElem.style.top = topN + 'px';
+        }
+        if (event.code === 'ArrowLeft') {
+            leftN = leftN - 10;
+           this.newElem.style.left = leftN + 'px';
+        }
+        if (event.code === 'ArrowRight') {
+            leftN = leftN + 10;
+           this.newElem.style.left = leftN + 'px';
+        }
+    });
+};
 
-backgroundBtn.addEventListener('click', function(event) {
-    event.preventDefault();
-    random();
-});
+let obj = new DomElement(100, 100);
+obj.render();
